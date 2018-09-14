889b6588bc04aa24d53918e3ce7fbbddc6f3b060
Tulha_Purdue <hochst10@purdue.edu>
2018-09-14 17:11:04 -0400

    Update 4

--- tictactoe.py
+++ tictactoe.py
@@ -1,5 +1,5 @@
 #First Small Project - Tic-Tac-Toe
-board = [['X','O','X'],['O','O','X'],['X','O','X']] #playable squares, will be filled throuoght the game
+board = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']] #playable squares, will be filled throuoght the game
 #board is filled for the purpose of testing table printer
 
 def printTable(matrix):
@@ -7,7 +7,7 @@ def printTable(matrix):
     bar = ('===================')
     row = 0
 
-    for i in range(0,6):
+    for i in range(0,7):
         if i % 2 == 0:
             print (bar)
         else:
@@ -17,7 +17,13 @@ def printTable(matrix):
             print('|') #prints the final element of the line and goes to the next one
 
             row += 1
-    print(bar)
+
+def winChecker(matrix):
+    if(1==1):#insert something that verifies if the game is over
+        return True
+    else: #when the game isn't over, returns false and proceeds to the next play
+        return False
+
 #def game2players():
 #def game1player():
 
@@ -25,6 +31,9 @@ def printTable(matrix):
 choice = int(input("***Welcome to Tic-Tac-Toe***\n\nType 1 if you want to play!"))
 while (choice == 1):
     #game() planned system
+    gameType = int(input())
     choice = int(input("***Thank you for playing***\n\nType 1 if you want to play again!"))
 
 printTable(board)
+
+print(winChecker(board))
