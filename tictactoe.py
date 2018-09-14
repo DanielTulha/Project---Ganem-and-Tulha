c19778648e5190d65016875ad87bafa4b4d0fc39
Tulha_Purdue <hochst10@purdue.edu>
2018-09-14 11:57:16 -0400

    Tic Tac Toe - Update 1
    Now with the "printTable" function. Function prints the table with the symbols in the game as long as a matrix is provided. Seems to work properly.

--- tictactoe.py
+++ tictactoe.py
@@ -1,1 +1,33 @@
 #First Small Project - Tic-Tac-Toe
+board = [['X','O','X'],['O','O','X'],['X','O','X']] #playable squares, will be filled throuoght the game
+#board is filled for the purpose of testing table printer
+
+def printTable(matrix):
+    print()
+    bar = ('===================')
+    row = 0
+
+    for i in range(0,6):
+        if i % 2 == 0:
+            print (bar)
+        else:
+            symbolNumber = 0
+            for j in range(3):
+
+                    print ('|',end ='  %s  ' % board[row][symbolNumber])
+                    symbolNumber += 1
+
+            print('|')
+
+            row += 1
+    print(bar)
+#def game2players():
+#def game1player():
+
+
+choice = int(input("***Welcome to Tic-Tac-Toe***\n\nType 1 if you want to play!"))
+while (choice == 1):
+    #game() planned system
+    choice = int(input("***Thank you for playing***\n\nType 1 if you want to play again!"))
+
+printTable(board)
